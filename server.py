dd9fea745f7125d76c8ee9a2c93e18bebb87e90e
Fabian-Robert Stöter <mail@faroit.com>
2016-06-08 21:53:26 +0200

    initial version

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setup(18, GPIO.OUT)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('main.html')
+
+
+@app.route("/press")
+def update_lamp():
+    GPIO.output(18, True)
+    time.sleep(1)
+    GPIO.output(18, False)
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
