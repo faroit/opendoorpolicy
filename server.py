767600a323f1f550b0be2a9fdd4c55ae2f16d9d7
Nils Werner <nils.werner@audiolabs-erlangen.de>
2016-06-16 13:24:29 +0200

    Moved optional GPIO into its own class

--- server.py
+++ server.py
@@ -1,11 +1,29 @@
 from flask import Flask, redirect, render_template, url_for
 import time
-import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+
+class Door(object):
+    def __init__(self, pin):
+        try:
+            import RPi.GPIO as GPIO
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(pin, GPIO.OUT)
+            self.gpio = True
+        except ImportError:
+            self.gpio = False
+
+    def open(self, timeout):
+        if self.gpio:
+            GPIO.output(18, True)
+            time.sleep(timeout)
+            GPIO.output(18, False)
+        else:
+            print "opening door for %ds" % timeout
+            time.sleep(timeout)
+
 
 app = Flask(__name__)
+door = Door(18)
 
 
 @app.route('/')
@@ -14,11 +32,10 @@ def index():
 
 
 @app.route("/press")
-def open_door():
-    GPIO.output(18, True)
-    time.sleep(3)
-    GPIO.output(18, False)
+def press():
+    door.open(3)
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
